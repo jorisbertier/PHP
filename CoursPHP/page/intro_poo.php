c66e5dfed4939862f8daee34b9823e2ea054530f
Joris bertier <joris.bertier@gmail.com>
2023-06-12 09:17:03 +0200

    ajout poo

--- CoursPHP/page/intro_poo.php
+++ CoursPHP/page/intro_poo.php
@@ -1,12 +1,48 @@
 <?php include_once __DIR__ . '/../component/header.php' ?>
-<?php include_once __DIR__ . '/../model/Vehicule.php' ?>
+<?php include_once __DIR__ . '/../../model/Vehicule.php' ?>
 
 
 
 <h1>Programmation orienté objet - Introduction</h1>
 
 <?php 
-$toto = new Vehicule();
+$vehicule1 = new Vehicule('brown', 10);
+
+// echo $vehicule1->getColor();
+
+// $vehicule1->setColor('red');
+// echo $vehicule1->getColor();
+
+// echo "Mon véhicule a " . $vehicule1->getWheelNb() . " roues.";
+
+$mario = new Personnage('mario', 10);
+$bowser = new Personnage('bowser', 10);
+
+$bowser->setHp(500);
+$bowser->setHpMax(500);
+$bowser->setStrength(50);
+echo "<pre>";
+// var_dump($mario);
+// echo "<pre>";
+
+// echo "<pre>";
+// var_dump($bowser);
+// echo "<pre>";
+
+echo $mario->getInfo();
+echo "<br>";
+echo $bowser->getInfo();
+echo "<br>";
+
+
+while($bowser->getHp() > 0) {
+    $mario->frapper($bowser);
+}
+
+if($bowser->getHp()< 0) {
+    echo "<br> bowser est ko";
+}
+
 ?>
 
 <?php include_once __DIR__ . '/../component/footer.php' ?>
--- model/Vehicule.php
+++ model/Vehicule.php
@@ -2,6 +2,91 @@
 
 
 class Vehicule {
-    private string $color = "blue";
+
+    //private
+    //public
+    //protected
+    private string $color;
+    private int $wheelNb;
+
+    public function __construct(string $newColor, int $newWheelNb) {
+        $this->color = $newColor;
+        $this->wheelNb = $newWheelNb;
+    }
+
+    public function getColor() : string {
+        return $this->color;
+    }
+
+    public function setColor($newColor) : void {
+        $this->color = $newColor;
+    }
+
+    
+    public function getWheelNb() : int {
+        return $this->wheelNb;
+    }
+
+    public function setWheelNb(int $wheelNb) : void {
+        $this->wheelNb = $wheelNb;
+    }
+
+}
+
+
+class Personnage {
+
+    private string $name;
+    private int $hpMax = 300;
+    private int $hp = 300;
+    private int $age;
+    private int $strength = 25;
+
+    public function __construct(string $newName, int $newAge) {
+        $this->name = $newName;
+        $this->age = $newAge;
+    }
+
+
+    public function getHpMax() : int {
+        return $this->hpMax;
+    }
+
+    public function setHpMax($newHpMax) : void {
+        $this->hpMax = $newHpMax;
+    }
+
+
+    public function getHp() : int{
+        return $this->hp;
+    }
+
+    public function setHp($newHp) : void {
+        $this->hp = $newHp;
+    }
+
+
+    public function getStrength() : int {
+        return $this->strength;
+    }
+
+    public function setStrength($newStrength) : void {
+        $this->strength = $newStrength;
+    }
+
+    public function getInfo() {
+        return $this->name." : ".$this->hp."/".$this->hpMax;
+    }
+
+    public function frapper(Personnage $personnage) {
+        $attack = $this->strength * rand(1,4);
+        $personnage->setHp($personnage->getHp() - $attack);
+
+        if($personnage->getHp() < 0){
+            $personnage->setHp(0);
+        }
+        echo $personnage->name . ' a subit ' . $attack . ', il lui reste ' . $personnage->getHp(). "<br>";
+    }
+
 }
 ?>
